6eea55d6d5fb01e062ec51ee7cc4ee7103fb3b9b
Bucataru Anton <anton.bucataru@gmail.com>
2024-12-05 13:07:06 +0100

    tmp

--- src/NeuralNetworkKernel.cl
+++ src/NeuralNetworkKernel.cl
@@ -12,7 +12,7 @@ __kernel void initialize_weights_and_biases(
 
 
         weights[id] = sin((id + seed) * 12.9898);  // Random number between -1 and 1 using sine
-        weights[id] = fmod(weights[id], 1.0);  // Normalize between 0 and 1
+        weights[id] = fmod(weights[id], (double)1.0);  // Normalize between 0 and 1
 
         if(id > 203260) printf("weight value for layer 3: %f\n", weights[id]);
 
@@ -25,7 +25,7 @@ __kernel void initialize_weights_and_biases(
     }
     if (id < num_biases){
         biases[id] = sin((id + seed) * 11.74932);  // Random number between -1 and 1 using sine
-        biases[id] = fmod(biases[id], 1.0);  // Normalize between 0 and 1
+        biases[id] = fmod(biases[id], (double)1.0);  // Normalize between 0 and 1
 
         if(biases[id] < -1){
             biases[id] = -1.0;
--- src/feedForward.cpp
+++ src/feedForward.cpp
@@ -6,28 +6,26 @@ struct Neuron {
 #pragma pack(pop)
 
 __kernel void feed_forward_cl(
-        __global struct Neuron* neurons, // current layer neurons
-        __global double* biases,  // biases for the current layer
-        __global double* biasWeights, //weights for all the biases
-        __global double* input, //weights for all the biases
-        __global double* weights, // weights between current and previous layer
-        __global struct Neuron* output,        // output to store calculated values
+        __global struct Neuron *neurons, // current layer neurons
+        __global double *biases,  // biases for the current layer
+        __global double *biasWeights, //weights for all the biases
+        __global double *input, //weights for all the biases
+        __global double *weights, // weights between current and previous layer
+        __global struct Neuron *output,        // output to store calculated values
         int num_neurons,                // neurons in previous layer
         int layer_id                    // current layer ID
-)
-{
+) {
     int id = get_global_id(0); // Get the global thread ID
 
     double sum = 0.0;
     // Handle input layer separately
     if (layer_id == 0) {
         output[id].value = input[id];
-    }
-    else{
+    } else {
         // Weighted sum computation
         for (int i = 0; i < num_neurons; i++) {
             sum += neurons[i].value * weights[id * num_neurons + i];
-            if(layer_id == 2) printf("weight layer 2: %f\n", weights[id * num_neurons + i]);
+            if (layer_id == 2) printf("weight layer 2: %f\n", weights[id * num_neurons + i]);
         }
 
 
@@ -35,9 +33,9 @@ __kernel void feed_forward_cl(
         sum += biases[id] * biasWeights[id];
 
         // Apply activation function (ReLU in this example)
-        double inter = fmax(sum, 0.0);
-        output[id].value = fmax(sum, 0.0); // ReLU activation
-        if(layer_id == 2){
+        double inter = fmax(sum, (double) 0.0);
+        output[id].value = fmax(sum, (double)0.0); // ReLU activation
+        if (layer_id == 2) {
             //printf("sum value layer 3: %f\n", sum);
             printf("intermediate value layer 3: %f\n", inter);
             printf("output value layer 3: %f\n", output[id].value);
